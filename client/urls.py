3f936e50659192147150dfa0d03162f471ed3602
mohiuddin_hemu <mhemu95@gmail.com>
2022-12-05 02:46:38 +0600

    added client add view. up next client edit and delete view

--- client/urls.py
+++ client/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.client_list, name='clients_list'),
     path('detail/<int:pk>/', views.client_detail, name='client_detail'),
+    path('add/', views.add_client, name='add_client'),
     
 ]
--- client/views.py
+++ client/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+from .forms import AddClientForm
 from . models import Client
 
 
@@ -17,3 +19,23 @@ def client_detail(request, pk):
     client = get_object_or_404(Client, created_by=request.user, pk=pk)
 
     return render(request, 'client/client_detail.html', {'client':client})
+
+
+@login_required
+def add_client(request):
+    if request.method == 'POST':
+        form = AddClientForm(request.POST)
+
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.created_by = request.user
+            client.save()
+
+            messages.success(request, 'Client created successfully')
+
+            return redirect(client_list)
+
+    else:
+        form = AddClientForm()
+
+    return render(request, 'client/add_client.html', {'form':form})
